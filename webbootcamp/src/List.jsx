d89f6b9612af89ea0328696c15dc73aa57384108
HIMANSHU3107 <himanshubulani07@gmail.com>
2021-04-24 21:17:10 +0530

    components

---
+++ webbootcamp/src/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class List extends React.Component{
+    render(){
+        return(
+            <ol>
+                <li>Welcome </li>
+                <li>To</li>
+                <li>Web</li>
+                <li>Bootcamp</li>
+            </ol>
+        )
+    }
+}
+export default List;
